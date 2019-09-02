839cad4b4b59f06b697ee8bbb59f4f2abf16517a
Chahkiev <chahkiev98@mail.ru>
2019-09-02 12:29:01 +0300

    made carcass

---
+++ server/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+from server.views import *
+
+urlpatterns = [
+
+    url(r'^users/add', addUser, name="addUser"),
+
+    url(r'^chats/add', addChat, name="addChat"),
+    url(r'^messages/add', addMessage, name="addMessage"),
+
+    url(r'^chats/get', getChats, name="getChats"),
+    url(r'^messages/get', getMessages, name="getMessages"),
+]
---
+++ server/views.py
@@ -0,0 +1,117 @@
+import ast
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+def add_user(user):
+    print('add_user()')
+    return
+
+
+def add_chat(chatName, users):
+    print('add_chat()')
+    return
+
+
+def add_message(chatId, userId, text):
+    print('add_message')
+    return
+
+
+def get_chats(userId):
+    print('get_chats()')
+    return
+
+
+def get_messages(chatId):
+    print('get_messages()')
+    return
+
+
+@csrf_exempt
+def addUser(request):
+    body = request.body
+    print(body)
+    body = body.decode("utf-8")
+    data = ast.literal_eval(body)
+    print(data)
+    print(data['username'])
+
+    if 'username' in data:
+        add_user(data['username'])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=409)
+
+
+
+@csrf_exempt
+def addChat(request):
+    body = request.body
+    print(body)
+    body = body.decode("utf-8")
+    data = ast.literal_eval(body)
+    print(data)
+    print(data['name'])
+    print(data['users'])
+    if 'name' in data and 'users' in data:
+        add_chat(data['name'], data['users'])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=409)
+    
+
+
+@csrf_exempt
+def addMessage(request):
+    body = request.body
+    print(body)
+    body = body.decode("utf-8")
+    data = ast.literal_eval(body)
+    print(data)
+    print(data['chat'])
+    print(data['author'])
+    print(data['text'])
+
+    if 'chat' in data and 'author' in data and 'text' in data:
+        add_message(data['chat'], data['author'], data['text'])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=409)
+    
+
+@csrf_exempt
+def getChats(request):
+    body = request.body
+    print(body)
+    body = body.decode("utf-8")
+    data = ast.literal_eval(body)
+    print(data)
+    print(data['user'])
+
+    if 'user' in data:
+        get_chats(data['user'])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=409)
+    return HttpResponse(status=200)
+
+
+@csrf_exempt
+def getMessages(request):
+    body = request.body
+    print(body)
+    body = body.decode("utf-8")
+    data = ast.literal_eval(body)
+    print(data)
+    print(data['chat'])
+
+    if 'chat' in data:
+        get_messages(data['chat'])
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=409)
+    return HttpResponse(status=200)
