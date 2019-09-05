3dd785554779801f958ed74fb49914b3f020505a
Chahkiev <chahkiev98@mail.ru>
2019-09-05 17:36:37 +0300

    deleted DEBUG prints, refactored some code

--- server/urls.py
+++ server/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from server.views import *
 
 urlpatterns = [
-    url(r'^printDB', print_DB, name="print Database"),
-
     url(r'^users/add', addUser, name="addUser"),
-
     url(r'^chats/add', addChat, name="addChat"),
     url(r'^messages/add', addMessage, name="addMessage"),
 
--- server/views.py
+++ server/views.py
@@ -1,40 +1,11 @@
 import ast
 from django.utils import timezone
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from server.models import User, Chat, ChatUser, Message
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-def print_DB(request):
-    print("print_DB()")
-    users = User.objects.all()
-    for i in users:
-        print(i.id, i.username, i.created_at)
-    
-    print()
-
-    chats = Chat.objects.all()
-    for i in chats:
-        print(i.id, i.name, i.created_at)
-
-    print()
-
-    chatUser = ChatUser.objects.all()
-    for i in chatUser:
-        print(i.id, i.chat, i.user)
-
-    print()
-
-    messages = Message.objects.all()
-    for i in messages:
-        print(i.id, i.chat, i.author, i.text, i.created_at)
-
-    print()
-
-    return HttpResponse(status=200)
 
 def user_exists(username):
     user = User.objects.filter(username=username).first()
@@ -48,12 +19,10 @@ def add_users_to_chat(chat, users):
     for userId in users:
         user = User.objects.filter(id=userId).first()
         userExistsInChat = ChatUser.objects.filter(chat=chat, user=userId).first()
-        print("userExistsInChat", userExistsInChat==None)
+
         if user != None and userExistsInChat == None:
             chatUserObj = ChatUser.objects.create(chat=chat, user=user)
-            print(chatUserObj)
             chatUserObj.save()
-
     return
 
 
@@ -66,7 +35,6 @@ def chat_exists(chatName):
 
     
 def add_user(username):
-    print('add_user()')
     userExists = user_exists(username)
     if not userExists:
         try:
@@ -74,7 +42,6 @@ def add_user(username):
                                     created_at=timezone.now())
             userObj.save()
             userId = User.objects.filter(username=username).first().id
-            print("userId",  userId)
             return {"userId": userId, "status" : 200}
         except:
             return {"userId": None, "status" : 409}
@@ -97,7 +64,6 @@ def add_chat(chatName, users):
         except:
             return {"chatId": None, "status" : 409}
     else:
-        print(chatExists)
         chat = Chat.objects.filter(name=chatName).first()
         add_users_to_chat(chat, users)
         return {"chatId": chatExists, "status" : 200}
@@ -107,9 +73,7 @@ def add_message(chatId, userId, text):
     print('add_message')
     chat = Chat.objects.filter(id=chatId).first()
     author = User.objects.filter(id=userId).first()
-    print(chat)
-    print(author)
-    print(text)
+
     messageObj = Message.objects.create(chat=chat,
                                         author=author,
                                         text=text,
@@ -129,7 +93,6 @@ def get_chats(userId):
             chats.append( [chatUserDB.chat, message.created_at.created_at.now().strftime("%Y-%m-%d %H:%M:%S")] )
             break
 
-    print(chats)
     return
 
 
@@ -140,23 +103,18 @@ def get_messages(chatId):
 
     for message in messagesDB:
         messages.append( [message.chat.name, message.author.username, message.text, message.created_at.now().strftime("%Y-%m-%d %H:%M:%S")] )
-        print(message.chat, message.author, message.text, message.created_at.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print(messages)
     return
 
 
 @csrf_exempt
 def addUser(request):
     body = request.body
-    print(body)
     body = body.decode("utf-8")
     data = ast.literal_eval(body)
-    print(data)
-    print(data['username'])
+    print("addUser(),  data: {}".format(data))
 
     if 'username' in data:
         user = add_user(data['username'])
-        print("returning Id ", HttpResponse( user["userId"] ).content)
         return HttpResponse( user["userId"] , status=user["status"])
     else:
         return HttpResponse(status=409)
@@ -165,15 +123,12 @@ def addUser(request):
 @csrf_exempt
 def addChat(request):
     body = request.body
-    print(body)
     body = body.decode("utf-8")
     data = ast.literal_eval(body)
-    print(data)
-    print(data['name'])
-    print(data['users'])
+    print("addChat(),  data: {}".format(data))
+
     if 'name' in data and 'users' in data and data['users'] != []:
         chat = add_chat(data['name'], data['users'])
-        print("returning Id ", HttpResponse( chat["chatId"] ).content)
         return HttpResponse( chat["chatId"] , status=chat["status"])
     else:
         return HttpResponse(status=409)
@@ -182,13 +137,9 @@ def addChat(request):
 @csrf_exempt
 def addMessage(request):
     body = request.body
-    print(body)
     body = body.decode("utf-8")
     data = ast.literal_eval(body)
-    print(data)
-    print(data['chat'])
-    print(data['author'])
-    print(data['text'])
+    print("addMessage(),  data: {}".format(data))
 
     chatExists = Chat.objects.filter(id=data['chat']).first()
     authorExists = User.objects.filter(id=data['author']).first()
@@ -205,11 +156,9 @@ def addMessage(request):
 @csrf_exempt
 def getChats(request):
     body = request.body
-    print(body)
     body = body.decode("utf-8")
     data = ast.literal_eval(body)
-    print(data)
-    print(data['user'])
+    print("getChats(),  data: {}".format(data))
 
     if 'user' in data:
         get_chats(data['user'])
@@ -222,11 +171,9 @@ def getChats(request):
 @csrf_exempt
 def getMessages(request):
     body = request.body
-    print(body)
     body = body.decode("utf-8")
     data = ast.literal_eval(body)
-    print(data)
-    print(data['chat'])
+    print("getMessages(),  data: {}".format(data))
 
     if 'chat' in data:
         get_messages(data['chat'])
